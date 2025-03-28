5d8ff14bc06b892c3b5bc547608152b8631af6ec
Brandon Stiffler <BrandonSt@beckhoff.com>
2025-03-27 22:09:21 -0400

    fixed indentations

--- makeAwaitable.js
+++ makeAwaitable.js
@@ -1,34 +1,34 @@
 // transform an async function which relies on a callback into a Promise-based function
 // so it can be called with async/await
 
+// definition, relies on callback
+function delayAdd(a, b, callbackFn) {
+  setTimeout(function() {
+    callbackFn(a + b);
+  }, 1000);
+}
+
 // make functions with callbacks awaitable
 function makeAwaitable(fnWithCallback) {
-    return function (...args) {
-      return new Promise((resolve, reject) => {
-          fnWithCallback(...args, (callback) => resolve(callback));
-      });
-    };
-  }
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+        fnWithCallback(...args, (callback) => resolve(callback));
+    });
+  };
+}
+
+// normal implementation using callback
+delayAdd(5, 5, function(data) {
+  console.log(data); // 10
+});
+
+// made awaitable
+(async () => {
+  const awaitableAdd = makeAwaitable(delayAdd);
+  let res = await awaitableAdd(6, 5); // 11
+  console.log(res);
   
-  // definition, relies on callback
-  function delayAdd(a, b, callbackFn) {
-      setTimeout(function() {
-        callbackFn(a + b);
-    }, 1000);
-  }
-  
-  // normal implementation using callback
-  delayAdd(5, 5, function(data) {
-    console.log(data); // 10
-  });
-  
-  // made awaitable
-  (async () => {
-    const awaitableAdd = makeAwaitable(delayAdd);
-    let res = await awaitableAdd(6, 5); // 11
-    console.log(res);
-    
-    // or with immediate invocation
-      res = await makeAwaitable(delayAdd)(7, 5);
-    console.log(res); // 12
-  })();
+  // or with immediate invocation
+  res = await makeAwaitable(delayAdd)(7, 5);
+  console.log(res); // 12
+})();
